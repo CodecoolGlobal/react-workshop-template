95ce10ce19cb6d10de1c8ecf6c497d385f96ff6a
Daniel Hatas <daniel.hatas@codecool.com>
2023-02-21 09:15:38 +0100

    initial commit

---
+++ src/data.js
@@ -0,0 +1,144 @@
+const getBeers = () => ([
+  {
+    brewery: "71 Brewing",
+    name: "Rivet red",
+    type: ["Ale", "Red"],
+    score: 649,
+    abv: 7
+  },
+  {
+    brewery: "Right Proper",
+    name: "Raised By Wolves",
+    type: ["Ale", "Pale", "American"],
+    score: 843,
+    abv: 5
+  },
+  {
+		brewery: "3 Floyds Brewing",
+		name: "Zombie Dust",
+        type: ["Ale", "Pale", "American"],
+		score: 960,
+		abv: 6.5
+	},
+  {
+    brewery: "Whalers",
+    name: "Rise",
+    type: ["Ale", "Pale", "American"],
+    score: 924,
+    abv: 6.5
+  },
+  {
+    brewery: "Half Acre",
+    name: "Daisy Cutter",
+    type: ["Pale", "Ale"],
+    score: 980,
+    abv: 5.2
+  },
+  {
+    brewery: "Main St",
+    name: "Naked Fox",
+    type: ["IPA"],
+    score: 607,
+    abv: 6.8
+  },
+  {
+    brewery: "Dogfish Head",
+    name: "90 Minute",
+    type: ["IPA", "Imperial"],
+    score: 606,
+    abv: 9
+  },
+  {
+    brewery: "Night Shift",
+    name: "The Whole Nine Years",
+    type: ["IPA", "Double", "American"],
+    score: 589,
+    abv: 8
+  },
+  {
+    brewery: "Anderson Valley",
+    name: "Hop Ottin",
+    type: ["IPA", "American"],
+    score: 771,
+    abv: 7
+  },
+  {
+    brewery: "Voodoo Ranger",
+    name: "Juice Force",
+    type: ["IPA", "Imperial", "Hazy"],
+    score: 921,
+    abv: 9.5
+  },
+  {
+    name: "Mango Bay",
+    brewery: "Mad Scientist Beer",
+    type: ["Pale Ale", "American"],
+    score: 521,
+    abv: 5.2
+  },
+  {
+    name: "Távoli Galaxis",
+    brewery: "Rothbeer Brewery",
+    type: ["IPA", "American"],
+    score: 671,
+    abv: 6.1
+  },
+  {
+    name: "Flying Rabbit",
+    brewery: "MONYO Brewing Co.",
+    type: ["IPA", "American"],
+    score: 861,
+    abv: 6.5
+  },
+  {
+    name: "Liquid Cocaine",
+    brewery: "Mad Scientist Beer",
+    type: ["IPA", "Imperial"],
+    score: 998,
+    abv: 9
+  },
+  {
+    name: "Organic Chocolate Stout",
+    brewery: "Samuel Smith Old Brewery",
+    type: ["Stout", "English"],
+    score: 512,
+    abv: 5
+  },
+  {
+    name: "Bracia",
+    brewery: "Thornbridge Brewery",
+    type: ["Ale", "English"],
+    score: 721,
+    abv: 10
+  },
+  {
+    name: "Oatmeal Stout",
+    brewery: "Samuel Smith Old Brewery",
+    type: ["Stout", "Oatmeal"],
+    score: 687,
+    abv: 5
+  },
+  {
+    name: "Black Tokyo Horizon",
+    brewery: "BrewDog",
+    type: ["Stout", "American", "Imperial"],
+    score: 929,
+    abv: 17.2
+  },
+  {
+    name: "Vintage Ale",
+    brewery: "Fuller's",
+    type: ["Old", "Ale"],
+    score: 491,
+    abv: 8.5
+  },
+  {
+    name: "All the Leaves are Brown",
+    brewery: "Tempest Brewing Company",
+    type: ["Brown", "Ale", "American"],
+    score: 534,
+    abv: 11.2
+  },
+]);
+
+module.exports = getBeers
